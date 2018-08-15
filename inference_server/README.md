4b690fb94a802ff2b896d56b731646dc373e1ff8
Alvaro Arcos <arcos1989@gmail.com>
2018-08-15 12:34:48 +0200

    Inference server

---
+++ inference_server/README.md
@@ -0,0 +1,47 @@
+# Inference server
+## Dependencies
+Library dependencies for the python code.  You need to install these with
+`pip install -r requirements.txt` before you can run this.
+## How to run it
+### Download model
+The model to perform document classification has been hosted in Google Drive. You can download it from: https://drive.google.com/file/d/1wJUnkFiqmwok1gJ2sKHPvJIaNjfT4pk6/view?usp=sharing
+Please, move the downloaded file **model.hdf5** to the **inference_server** folder.
+### Environment variables
+Set the following environment variables:
+| Parameter         | Environment Variable | Default Value           |
+|-------------------|----------------------|-------------------------|
+| number of workers | MODEL_SERVER_WORKERS | the number of CPU cores |
+| timeout           | MODEL_SERVER_TIMEOUT | 120 seconds             |
+| nginx config path | NGINX_CONF_PATH      | /etc/nginx/nginx.conf   |
+Example:
+```bash
+export MODEL_SERVER_WORKERS=1
+export MODEL_SERVER_TIMEOUT=120
+export NGINX_CONF_PATH=/home/user/UiPath_Document_Classification/inference_server/nginx.conf
+```
+### Run the inference server
+By default, this server uses the port 1234. Run it with the following command:
+```bash
+sudo -E ./serve
+```
+### Time to try it!
+Send a file to the server using curl:
+```bash
+curl -X POST -F "file=@PATH_TO_YOUR_FILE" "http://localhost:1234/document_classification"
+```
+Result:
+```json
+{
+    "prediction": {
+        "confidence": "1.0",
+        "class": "invoice"
+    },
+    "confidences": {
+        "invoice": "1.000",
+        "passport": "0.000",
+        "id_card_2": "0.000",
+        "driving_licence": "0.000",
+        "id_card_3": "0.000"
+    }
+}
+```
---
+++ inference_server/predictor.py
@@ -0,0 +1,97 @@
+import numpy as np
+from PIL import Image
+import flask
+from pdf2image import convert_from_bytes
+import tensorflow as tf
+from keras.models import load_model
+import json
+import io
+
+IMAGE_SIZE = 224
+ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+
+def get_file_extension(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower()
+
+
+idx2label = {0: 'driving_licence',
+             1: 'id_card_2',
+             2: 'id_card_3',
+             3: 'invoice',
+             4: 'passport'}
+
+
+# A singleton for holding the model. This simply loads the model and holds it.
+# It has a predict function that does a prediction based on the model and the input data.
+
+class ClassificationService(object):
+    model = None
+    graph = None
+
+    @classmethod
+    def get_model(cls):
+        if cls.model is None:
+            cls.model = load_model('model.hdf5')
+            cls.graph = tf.get_default_graph()
+        return cls.model
+
+    @classmethod
+    def get_graph(cls):
+        if cls.graph is None:
+            cls.get_model()
+        return cls.graph
+
+    @classmethod
+    def predict(cls, image):
+        with cls.get_graph().as_default():
+            predictions = np.squeeze(cls.get_model().predict(image))
+            confidences = {}
+            for idx, prediction in enumerate(predictions):
+                confidences[idx2label[idx]] = '{:.3f}'.format(prediction)
+            pred = {'class': idx2label[np.argmax(predictions)], 'confidence': str(np.max(predictions))}
+            result = {
+                'prediction': pred,
+                'confidences': confidences
+            }
+            json_result = json.dumps(result)
+            return json_result
+
+
+# The flask app for serving predictions
+app = flask.Flask(__name__)
+
+
+@app.route('/ping', methods=['GET'])
+def ping():
+    """Determine if the container is working and healthy. In this sample container, we declare
+    it healthy if we can load the model successfully."""
+    health = ClassificationService.get_model() is not None
+    status = 200 if health else 404
+    return flask.Response(response='\n', status=status, mimetype='application/json')
+
+
+@app.route('/document_classification', methods=['POST'])
+def document_classification():
+    if flask.request.files.get('file'):
+        file = flask.request.files['file']
+        if file and get_file_extension(file.filename) in ALLOWED_EXTENSIONS:
+            if get_file_extension(file.filename) == 'pdf':
+                pdf = file.read()
+                images = convert_from_bytes(pdf, fmt='jpg')
+                image = images[0]
+            else:
+                image = Image.open(io.BytesIO(file.read()))
+            image = image.resize((IMAGE_SIZE, IMAGE_SIZE), resample=Image.NEAREST)
+            image = np.array(image).astype(np.float32)
+            image = image / 255
+            image = np.expand_dims(image, axis=0)
+            classification_service = ClassificationService()
+            js = classification_service.predict(image)
+            return flask.Response(response=js, status=200, mimetype='application/json')
+        else:
+            return flask.Response(response='File extension not allowed. Allowed:[pdf, png, jpg, jpeg, gif]',
+                                  status=415, mimetype='text/plain')
+    else:
+        return flask.Response(response='Parameter [file] is required.', status=415, mimetype='text/plain')
